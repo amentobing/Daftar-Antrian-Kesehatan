808d521a1ff03a07d47a1b098f4c206a8cec2530
Mamen <53280611+amentobing@users.noreply.github.com>
2024-11-08 10:46:40 +0000

    update

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template
 from markupsafe import escape
 app = Flask(__name__)
 
-appName = "Aplikasi Daftar Antrian - by Amen Togu [50423150]"
+appSName = "Aplikasi Daftar Antrian"
+appName = f"{appSName} - by Amen Togu [50423150]"
 appDesc = f"\"{appName}\" yang diperuntukkan untuk Project Informatika Kesehatan. Aplikasi ini berguna untuk membuat daftar antrian untuk fasilitas kesehatan seperti Puskesmas, Rumah Sakit, dan fasilitas kesehatan lainnya."
 
 
 @app.route("/")
 def home():
-    return render_template('home.html', appName=appName, appDesc=appDesc)
+    return render_template('home.html', appName=appName, appDesc=appDesc, appSName=appSName)
 
 
 @app.route("/sayHI/<name>")
