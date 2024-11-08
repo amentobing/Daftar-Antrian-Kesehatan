3556bfbf44e4804afa1f2ca681396d8b53f9778b
Mamen <53280611+amentobing@users.noreply.github.com>
2024-11-08 09:39:36 +0000

    initial app and view model

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from markupsafe import escape
+app = Flask(__name__)
+
+appName = "Aplikasi Daftar Antrian - by Amen Togu [50423150]"
+appDesc = f"\"{appName}\" yang diperuntukkan untuk Project Informatika Kesehatan. Aplikasi ini berguna untuk membuat daftar antrian untuk fasilitas kesehatan seperti Puskesmas, Rumah Sakit, dan fasilitas kesehatan lainnya."
+
+
+@app.route("/")
+def home():
+    return render_template('home.html', appName=appName, appDesc=appDesc)
+
+
+@app.route("/sayHI/<name>")
+def sayHI(name):
+    return f"<h1> Hi, {escape(name)}! </h1>"
